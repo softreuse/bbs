01c9c2591e5251e2d8d26c719014b7fed3dd0c71
cheneyveron <cheneyveron@live.cn>
2016-12-01 19:56:52 +0800

    Init spring boot

---
+++ src/main/java/cn/com/youdab/YoudabApplication.java
@@ -0,0 +1,12 @@
+package cn.com.youdab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoudabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoudabApplication.class, args);
+	}
+}
---
+++ src/test/java/cn/com/youdab/YoudabApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.com.youdab;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YoudabApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
